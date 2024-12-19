7fccd21ade7936378a0ae611bb97a1d5ed534a1e
a-jednak <zjedynak@student.agh.edu.pl>
2024-12-19 19:16:02 +0100

    main

--- Darwin_World/src/main/java/agh/ics/oop/Main.java
+++ Darwin_World/src/main/java/agh/ics/oop/Main.java
@@ -2,11 +2,4 @@ package agh.ics.oop;
 
 public class Main {
 
-    public static void main(String[] args) {
-        float a = 30f;
-        float b = 70f;
-        float c = (b-a)/(80*100);
-
-        System.out.println(c);
-    }
 }
